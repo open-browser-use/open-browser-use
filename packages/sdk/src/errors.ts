2c0dee113ef70313938f9c6bfa51f5142dbb0ae1
labrinyang <zhiweiy9125@gmail.com>
2026-05-25 00:58:32 +0800

    refactor(sdk): consume generated ERR_* in errors.ts

--- packages/sdk/src/errors.ts
+++ packages/sdk/src/errors.ts
@@ -1,4 +1,5 @@
 import { PRODUCT_ERROR_SCHEMA } from "./product_errors.generated.js";
+import { ERR_IO } from "./error_codes.generated.js";
 
 export class ObuError extends Error {
   constructor(
@@ -26,23 +27,9 @@ export class ObuError extends Error {
   }
 }
 
-export const ERR_TIMEOUT = -1000;
-export const ERR_NOT_FOUND = -1001;
-export const ERR_DISALLOWED = -1002;
-export const ERR_NOT_IMPLEMENTED = -1003;
-export const ERR_PROTOCOL = -1004;
-export const ERR_NO_BACKEND = -1005;
-export const ERR_OVERLOADED = -1006;
-export const ERR_CONFLICT = -1007;
-export const ERR_IO = -1099;
-export const ERR_PEER_AUTH = -1100;
-export const ERR_CAPABILITY_TOKEN = -1101;
-export const ERR_CMD_DISALLOWED = -1102;
-export const ERR_PAGE_CLOSED = -1200;
-export const ERR_CDP_FAILURE = -1201;
-export const ERR_TAB_NOT_ATTACHED = -1202;
-export const ERR_DIALOG_REQUIRES_DECISION = -1203;
+export * from "./error_codes.generated.js";
 
+// ERR_IO is imported as a local binding for the ERR_TRANSPORT_CLOSED alias below; "export *" re-exports the codes but does not bind them locally.
 export const ERR_TRANSPORT_CLOSED = ERR_IO;
 
 export type ProductErrorEntry = (typeof PRODUCT_ERROR_SCHEMA)[number];
